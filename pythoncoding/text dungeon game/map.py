67ada1df4891b97489fa8300c3ea226f5ca23f88
juraj panoc <juraj.panoc@gmail.com>
2024-04-23 18:19:58 +0200

    "updated the map.py file (work in progress), added a generate_branch method, added a generate_layout method, added a draw_map method"

--- pythoncoding/text dungeon game/map.py
+++ pythoncoding/text dungeon game/map.py
@@ -1,43 +1,119 @@
 # map / the level display and generation file
+from random import choice
 
-from random import randint, choice
 
 class Map:
-    def __init__(self, max_size: tuple) -> None:
-        self.max_width, self.max_height = max_size
-        self.rooms: list = [[0] * self.max_width] * self.max_height
-        self.treasure_sprite: str = "T"
-        self.enemy_sprite: str = "E"
-        self.side_wall: str = "│"
-        self.top_wall: str = "─"
+    def __init__(self, size: tuple[int, int]):
+        self.max_width, self.max_height = size
+        self.map_layout: list[list[int]] = [[0 for _ in range(self.max_width)] for _ in range(self.max_height)]
 
-    def generate_rooms(self):
-        ... # TODO: replace this with the functional version that you wrote in class
+    def generate_branch(self, x: int, y: int, room_count: int) -> list[tuple[int]]: #TODO: make it generate better maps, it currently looks too bunched up and ugly
+        """
+        generates a branch of rooms in a 2d array from its starting point,
+        x and y are the starting room coordinates
+        room_count is the amount of rooms it will generate (might generate less because it could crash into itself)
+        returns a list of points where more branches can sprout if needed
+        """
 
+        moves: list[str] = ["up", "down", "left", "right"]
+        branch_points: list[tuple[int]] = []
 
+        for i in range(room_count):
+            # disables movement if it goes out or the map or if it goes over a generated room
+            if y == 0:
+                moves.remove("up")
+            elif self.map_layout[y - 1][x]:
+                moves.remove("up")
+            if y == self.max_height - 1:
+                moves.remove("down")
+            elif self.map_layout[y + 1][x]:
+                moves.remove("down")
+            if x == 0:
+                moves.remove("left")
+            elif self.map_layout[y][x - 1]:
+                moves.remove("left")
+            if x == self.max_width - 1:
+                moves.remove("right")
+            elif self.map_layout[y][x + 1]:
+                moves.remove("right")
+            
+            if not moves:
+                print("it cornered itself") # temporary print
+                break
 
-    def random_special_rooms(self, encounter_count: int, treasure_count: int):
-        raise NotImplementedError("Add a random generated distribution of enemy encounters and treasures")
+            direction = choice(moves)
+            print(direction) # temporary print
+            
+            if direction == "up":
+                y -= 1
+            elif direction == "down":
+                y += 1
+            elif direction == "left":
+                x -= 1
+            elif direction == "right":
+                x += 1
 
-    def draw_room_row(self, row_number: int, content: list=None) -> None:
-        walls: list[str] = [f"┌─{self.top_wall}─┐", f"{self.side_wall}   {self.side_wall}", f"└─{self.top_wall}─┘"]
-        
-        if content == None:
-            content = [" "] * self.max_width # if its not defined then it makes everything empty
-        
-        for i in range(len(walls)):
-            for j in range(self.max_width):
-                if self.rooms[row_number][i]:
-                    print(walls[i], end="")
-                else:
-                    print(" " * len(walls[i]), end="")
-            print()
+            self.map_layout[y][x] = 1  # sets the room
+
+            if i % 2 == 0: # every even room can sprout a new branch
+                branch_points.append((x, y))
+            
+            for row in self.map_layout: # temporary way or printing the array (for debugging only)
+                print(row)
+            print("===============================")
+
+            moves = ["up", "down", "left", "right"]  # resets the possible moves
+
+        return branch_points #returns all possible points where the new branches can start
     
-    def draw_room_layout(self) -> None:
-        Map.generate_rooms(self)
-        for column in range(self.max_height):
-            Map.draw_room_row(self, column)
+    def generate_layout(self): #TODO: make the branch lengths scale off of the map size
+        """
+        generates a 2d array that determines the room layout,
+        2 is the player in a room,
+        1 is a room,
+        0 is an empty space,
+        """
 
-newmap = Map((5, 5))
+        main_branch_length: int = 5
+        extra_branch_length: int = 5
 
-newmap.draw_room_layout()
+        # starting room, set in the middle
+        x, y = self.max_width // 2, self.max_height // 2
+        self.map_layout[y][x] = 2 # player is set here
+
+        #main branch
+        branch_points = Map.generate_branch(self, x, y, main_branch_length)
+
+        #extra branches
+        for possible_branch in branch_points:
+            print("new branch being generated") # temporary print
+            Map.generate_branch(self, possible_branch[0], possible_branch[1], extra_branch_length)
+    
+    def draw_map(self) -> None:
+        """
+        prints out the whole map based on the map_layout,
+        returns None
+        """
+        
+        room_walls: list[str] = ["┌───┐", "│   │", "└───┘"]
+        player_sprite: str = "P"
+
+        for row in self.map_layout:
+            for i in range(3):
+                for column in row:
+                    if column == 2: # room occupied by the player
+                        room_walls[1] = f"│ {player_sprite} │"
+                        print(room_walls[i], end="")
+                        room_walls[1] = "│   │"
+                    elif column == 1:
+                        print(room_walls[i], end="") # normal room
+                    else:
+                        print("     ", end="") # no room
+                print()
+
+
+new_map = Map((10, 10))
+
+new_map.generate_layout()
+
+new_map.draw_map()
