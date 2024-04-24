9c43bae27b27ba032c4349c2fc3d67cc6900d861
juraj panoc <juraj.panoc@gmail.com>
2024-04-24 17:11:43 +0200

    "slightly improved the map generation (its still bad though)"

--- pythoncoding/text dungeon game/map.py
+++ pythoncoding/text dungeon game/map.py
@@ -2,12 +2,17 @@
 from random import choice
 
 
+def remove_all(array: list, item) -> list:
+    return [j for _,j in enumerate(array) if j!=item]
+
+
 class Map:
     def __init__(self, size: tuple[int, int]):
         self.max_width, self.max_height = size
         self.map_layout: list[list[int]] = [[0 for _ in range(self.max_width)] for _ in range(self.max_height)]
 
-    def generate_branch(self, x: int, y: int, room_count: int) -> list[tuple[int]]: #TODO: make it generate better maps, it currently looks too bunched up and ugly
+    def generate_branch(self, x: int, y: int, room_count: int) -> list[tuple[int]]: 
+        #TODO: make it generate better maps, less bunched up now but still too gobbed up, main problem is that it bunches itself up in one direction
         """
         generates a branch of rooms in a 2d array from its starting point,
         x and y are the starting room coordinates
@@ -21,21 +26,21 @@ class Map:
         for i in range(room_count):
             # disables movement if it goes out or the map or if it goes over a generated room
             if y == 0:
-                moves.remove("up")
+                moves = remove_all(moves, "up")
             elif self.map_layout[y - 1][x]:
-                moves.remove("up")
+                moves = remove_all(moves, "up")
             if y == self.max_height - 1:
-                moves.remove("down")
+                moves = remove_all(moves, "down")
             elif self.map_layout[y + 1][x]:
-                moves.remove("down")
+                moves = remove_all(moves, "down")
             if x == 0:
-                moves.remove("left")
+                moves = remove_all(moves, "left")
             elif self.map_layout[y][x - 1]:
-                moves.remove("left")
+                moves = remove_all(moves, "left")
             if x == self.max_width - 1:
-                moves.remove("right")
+                moves = remove_all(moves, "right")
             elif self.map_layout[y][x + 1]:
-                moves.remove("right")
+                moves = remove_all(moves, "right")
             
             if not moves:
                 print("it cornered itself") # temporary print
@@ -55,27 +60,24 @@ class Map:
 
             self.map_layout[y][x] = 1  # sets the room
 
-            if i % 2 == 0: # every even room can sprout a new branch
+            if i % 3 == 0: # every even room can sprout a new branch
                 branch_points.append((x, y))
             
             for row in self.map_layout: # temporary way or printing the array (for debugging only)
                 print(row)
             print("===============================")
 
-            moves = ["up", "down", "left", "right"]  # resets the possible moves
-
+            moves = ["up", "down", "left", "right"] # resets the possible moves
+            moves.append(direction) # adds the recent direction to make it more likely to go that way
         return branch_points #returns all possible points where the new branches can start
     
-    def generate_layout(self): #TODO: make the branch lengths scale off of the map size
+    def generate_layout(self, main_branch_length: int, extra_branch_length: int): #TODO: make the branch lengths scale off of the map size
         """
         generates a 2d array that determines the room layout,
         2 is the player in a room,
         1 is a room,
         0 is an empty space,
         """
-
-        main_branch_length: int = 5
-        extra_branch_length: int = 5
 
         # starting room, set in the middle
         x, y = self.max_width // 2, self.max_height // 2
@@ -112,8 +114,8 @@ class Map:
                 print()
 
 
-new_map = Map((10, 10))
+new_map = Map((15, 15))
 
-new_map.generate_layout()
+new_map.generate_layout(11, 5)
 
 new_map.draw_map()
