13b6aaf3bf1afe7da4ad0ccae8b77acad3d52586
juraj panoc <juraj.panoc@gmail.com>
2024-06-04 20:15:47 +0200

    added a create_boss_room method to Map class, added more class attributes to make code more readable, rewrote the draw_map method to use colors and prints more effectively, changed the player sprite, added a boss sprite,

--- pythoncoding/text dungeon game/map.py
+++ pythoncoding/text dungeon game/map.py
@@ -5,21 +5,67 @@ import battle_manager as bm
 
 class Map:
     room_walls: list[str] = ["┌───┐", "│   │", "└───┘"]
-    player_sprite: str = "P"
+    player_sprite: str = "■"
+    boss_sprite: str = "☠"  #note: the skull sprite doesn't want to cooperate (offset to the side)
     player_color: str = "light_blue"
+    current_color: str = "default"
+
+    no_room: str = "0"
+    normal_room: str = "1"
+    explored_room: str = "2"
+    boss_room: str = "3"
+    player_in_room: str = "9" 
 
     def __init__(self, width, height, max_room_count) -> None:
-        """
-        0 - no room
-        1 - normal room
-        2 - explored room
-        room with '9' at the end - room with the player
-        """
         self.max_room_count: int = max_room_count
         self.max_width, self.max_height = width, height
         self.map_layout: list[list[str]] = [["0" for _ in range(self.max_width)] for _ in range(self.max_height)]
         self.player_pos: tuple[int, int] = ()
         self.current_pos: tuple[int, int] = ()
+    
+    def move_player(self, direction: str) -> None:
+        """
+        moves the player,
+        prints 'cant move there' if its out of the map or out of the room layout,
+        returns: None
+        """
+        x, y = self.player_pos
+        self.map_layout[y][x] = self.explored_room
+        if direction == "w" and y > 0 and self.map_layout[y - 1][x] != self.no_room:
+            y -= 1
+        elif direction == "s" and y + 1 < self.max_height and self.map_layout[y + 1][x] != self.no_room:
+            y += 1
+        elif direction == "a" and x > 0 and self.map_layout[y][x - 1] != self.no_room:
+            x -= 1
+        elif direction == "d" and x + 1 < self.max_width and self.map_layout[y][x + 1] != self.no_room:
+            x += 1
+        else:
+            print("cant move there!")
+        
+        if self.map_layout[y][x] == self.normal_room: # player goes to an unexplored room
+            roll = randint(1, 3)
+            if roll == 1:
+                bm.battle_loop()
+        elif self.map_layout[y][x] == self.boss_room:
+            raise NotImplementedError("Implement bossfights") #TODO: add bossfights to the game
+
+        self.map_layout[y][x] += self.player_in_room
+        self.player_pos = x, y
+    
+    def create_boss_room(self):
+        """
+        finds the farthest room from the start and makes it into a boss room
+        """
+        player_x, player_y = self.player_pos
+        distances = []
+        distance_metric = lambda coords: (coords[0] - player_x)**2 + (coords[1] - player_y)**2
+        for y in range(len(self.map_layout)):
+            for x in range(len(self.map_layout[0])):
+                if self.map_layout[y][x] != self.no_room:
+                    distances.append((x, y))
+        
+        boss_loc = max(distances, key=distance_metric)
+        self.map_layout[boss_loc[1]][boss_loc[0]] = "3"
 
     def will_collide(self, pos: tuple[int, int], length: int, direction: str) -> bool:
         """
@@ -32,54 +78,27 @@ class Map:
             if length > y:
                 return True
             for i in range(1, length + 1):
-                if self.map_layout[y - i][x] != "0":
+                if self.map_layout[y - i][x] != self.no_room:
                     return True
         elif direction == "down":
             if length >= self.max_height - y:
                 return True
             for i in range(1, length + 1):
-                if self.map_layout[y + i][x] != "0":
+                if self.map_layout[y + i][x] != self.no_room:
                     return True
         elif direction == "left":
             if length > x:
                 return True
             for i in range(1, length + 1):
-                if self.map_layout[y][x - i] != "0":
+                if self.map_layout[y][x - i] != self.no_room:
                     return True
         elif direction == "right":
             if length >= self.max_width - x:
                 return True
             for i in range(1, length + 1):
-                if self.map_layout[y][x + i] != "0":
+                if self.map_layout[y][x + i] != self.no_room:
                     return True
         return False
-
-    def move_player(self, direction: str) -> None:
-        """
-        moves the player,
-        prints 'cant move there' if its out of the map or out of the room layout,
-        returns: None
-        """
-        x, y = self.player_pos
-        self.map_layout[y][x] = "2"
-        if direction == "w" and y > 0 and self.map_layout[y - 1][x] != "0":
-            y -= 1
-        elif direction == "s" and y + 1 < self.max_height and self.map_layout[y + 1][x] != "0":
-            y += 1
-        elif direction == "a" and x > 0 and self.map_layout[y][x - 1] != "0":
-            x -= 1
-        elif direction == "d" and x + 1 < self.max_width and self.map_layout[y][x + 1] != "0":
-            x += 1
-        else:
-            print("cant move there!")
-        
-        if self.map_layout[y][x] == "1": # player goes to an unexplored room
-            roll = randint(1, 3)
-            if roll == 1:
-                bm.battle_loop()
-        
-        self.map_layout[y][x] += "9"
-        self.player_pos = x, y
 
     def create_hall(self, pos: tuple[int, int], length: int, direction: str) -> None:
         """
@@ -99,7 +118,7 @@ class Map:
                     x -= 1
                 case "right":
                     x += 1
-            self.map_layout[y][x] = "1"
+            self.map_layout[y][x] = self.normal_room
             self.max_room_count -= 1
         self.current_pos = (x, y)
     
@@ -141,12 +160,12 @@ class Map:
         Note: larger "density" tends to slow down the generation of the map,
         returns: None
         """
-        origin = randint(1, self.max_width - 1), randint(1, self.max_height - 1)
-        self.player_pos = origin
-        self.map_layout[origin[1]][origin[0]] = "19"
+        self.origin = randint(1, self.max_width - 1), randint(1, self.max_height - 1)
+        self.player_pos = self.origin
+        self.map_layout[self.origin[1]][self.origin[0]] = self.normal_room + self.player_in_room
 
         for _ in range(4): # four branches stemming from the origin
-            self.current_pos = origin
+            self.current_pos = self.origin
             init_branches = self.create_halls(self.current_pos, halls_count=density)
             new_branches = []
             for branch in init_branches:
@@ -154,6 +173,7 @@ class Map:
             
             for new_branch in new_branches:
                 self.create_halls(new_branch, halls_count=density)
+        self.create_boss_room()
     
     def crop_map(self) -> None:
         """
@@ -185,9 +205,10 @@ class Map:
                 self.map_layout[i].pop(index)
         self.max_width = len(self.map_layout[0])
         
+        player_room_indicator = self.normal_room + self.player_in_room
         for i, row in enumerate(self.map_layout): # finds the players initial position
-            if "19" in row:
-                column_index = row.index("19")
+            if player_room_indicator in row:
+                column_index = row.index(player_room_indicator)
                 row_index = i
         self.player_pos = (column_index, row_index)
 
@@ -199,19 +220,24 @@ class Map:
         for row in self.map_layout:
             for i in range(3):
                 for column in row:
-                    if column.endswith("9"): # room occupied by the player
-                        self.room_walls[1] = f"│ {self.player_sprite} │"
-                        self.player_sprite = f"{cf.colors[self.player_color]}{self.player_sprite}{cf.colors["default"]}"
-                        if column[0] == "2":
-                            self.player_sprite = f"{cf.colors[self.player_color]}{self.player_sprite}{cf.colors["dark_grey"]}"
+                    if column[0] == self.normal_room:
+                        self.current_color = "default"
+                    elif column[0] == self.explored_room:
+                        self.current_color = "dark_grey"
+                    elif column[0] == self.boss_room:
+                        self.current_color = "red"
+                        self.room_walls[1] = f"│ {self.boss_sprite} │"
                     
-                    if column[0] == "1": # normal room
-                        print(self.room_walls[i], end="")
-                    elif column[0] == "2": # explored room
-                        print(f"{cf.colors["dark_grey"]}{self.room_walls[i]}{cf.colors["default"]}", end="")
-                    elif column[0] == "0":
-                        print("     ", end="") # no room
-                    self.room_walls[1] = "│   │" # defaults the content of the room (empty)
+                    if column.endswith(self.player_in_room):
+                        self.room_walls[1] = f"│ {self.player_sprite} │"
+                        self.player_sprite = f"{cf.colors[self.player_color]}{self.player_sprite}{cf.colors[self.current_color]}"
+
+                    if column[0] == self.no_room:
+                        print("     ", end="")
+                    else:
+                        print(f"{cf.colors[self.current_color]}{self.room_walls[i]}{cf.colors["default"]}", end="")
+                    
+                    self.room_walls[1] = "│   │" # resets the content of the room (empty)
                 print()
         print("-" * self.max_width * 5)
 
@@ -221,4 +247,3 @@ class Map:
 new_map = Map(30, 30, max_room_count = 20)
 new_map.generate_map(density=10)
 new_map.crop_map()
-
